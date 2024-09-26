2fdf37a1555336d71a62ef4b5104e1be97ad6283
Ria <ria.helle@live.com>
2024-09-26 11:27:19 +0300

    building fetch

--- Teht4/liikennekamera.js
+++ Teht4/liikennekamera.js
@@ -3,21 +3,27 @@ fetch('https://tie.digitraffic.fi/api/weathercam/v1/stations/C04507/data')
     return response.json();
 })
 .then(function(data) {
-    console.log(data); //Tulostaa datan konsoliin tarkistusta varten
-    liikennekamera(data); //Käsitellään data liikennekamera-funktiossa
+    console.log("Saapuva data: ", data); // Tulostetaan data tarkistusta varten
+    liikennekamera(data); // Käsitellään data liikennekamera-funktiossa
 })
 .catch(function(error) {
     document.getElementById("vastaus").innerHTML =
-    "<p>Tietoja ei pystyät hakemaan</p>" + error;
+    "<p>Tietoja ei pysty hakemaan</p>" + error;
 });
 
 function liikennekamera(data) {
-    var teksti="";
-    teksti = "<h1>Autolla Tampereella</h1>";
-    for (var i=0; i<data.length; i++) {
-        teksti = teksti + "<h3>" +data[i].title + "</h3>";
-        teksti = teksti + "<p>" + data[i].description + "</p>";
-        teksti = teksti + ""
+    var teksti = "<h1>Autolla Tampereella</h1>";
+    
+    // Tarkistetaan, että data sisältää presets-taulukon
+    if (data && data.presets && Array.isArray(data.presets)) {
+        teksti += "<p>Päivitetty: " + data.dataUpdatedTime + "</p>";
+        for (var i = 0; i < data.presets.length; i++) {
+            teksti += "<h3>Kamera ID: " + data.presets[i].id + "</h3>";
+            teksti += "<p>Mitattu aika: " + data.presets[i].measuredTime + "</p>";
+        }
+    } else {
+        teksti += "<p>Ei löydy kameradataa.</p>";
     }
+    
     document.getElementById("vastaus").innerHTML = teksti;
 }
