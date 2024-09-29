c6ba071b5c79e33e208ef83fce9cd32769db4eae
Ria <ria.helle@live.com>
2024-09-29 17:22:15 +0300

    kommenttien lisäystä

--- Teht 1-2/etusivu.js
+++ Teht 1-2/etusivu.js
@@ -12,20 +12,20 @@ fetch('https://raw.githubusercontent.com/rihell/Dig3JSON/refs/heads/master/Teht%
 
   function kerroEtusivu(data) {
     console.log(data);
-    var teksti = "<h1>" + data.otsikko + "</h1>";
-    teksti += "<p>" + data.kuvaus + "</p>";
-    teksti += "<p><img src='" + data.kuva + "' alt='kuva'></p>";
-    teksti += "<h3>Opintojakso: " + data.opintojakso + " " + data.tunnus + " " + data.opintopisteet + " op</h3>";
+    var teksti = "<h1>" + data.otsikko + "</h1>"; //Otsikko
+    teksti += "<p>" + data.kuvaus + "</p>"; //Kuvaus
+    teksti += "<p><img src='" + data.kuva + "' alt='kuva'></p>"; //Kuva
+    teksti += "<h3>Opintojakso: " + data.opintojakso + " " + data.tunnus + " " + data.opintopisteet + " op</h3>"; //Opintojakso
     
     teksti += "<ul>"; // Aloita lista
     for (var i = 0; i < data.sisalto.length; i++) {
-        teksti += "<li>" + data.sisalto[i] + "</li>";
+        teksti += "<li>" + data.sisalto[i] + "</li>"; //Lista totetuksen osa-alueista
     }
     teksti += "</ul>"; // Sulje lista
     
     teksti += "<h3>Linkit</h3><ul>"; // Aloita linkkilista
     for (var i = 0; i < data.tekniikat.length; i++) {
-        teksti += "<li>" + data.tekniikat[i].aihe + " - <a href='" + data.tekniikat[i].linkki + "'>" + data.tekniikat[i].linkki + "</a></li>";
+        teksti += "<li>" + data.tekniikat[i].aihe + " - <a href='" + data.tekniikat[i].linkki + "'>" + data.tekniikat[i].linkki + "</a></li>"; //Aiheet ja linkit
     }
     teksti += "</ul>"; // Sulje linkkilista
     
--- Teht 1-2/toteutus.js
+++ Teht 1-2/toteutus.js
@@ -11,19 +11,19 @@ fetch('https://raw.githubusercontent.com/rihell/Dig3JSON/refs/heads/master/Teht%
   });
 
 function kerroToteutus(data) {
-  var teksti = "<h1>Toteutus: " + data.toteutus.nimi + "</h1>";
-  teksti += "<p>Osallistujia: " + data.toteutus.osallistujat.lukumäärä + "</p>";
+  var teksti = "<h1>Toteutus: " + data.toteutus.nimi + "</h1>"; //Toteutuksen nimi
+  teksti += "<p>Osallistujia: " + data.toteutus.osallistujat.lukumäärä + "</p>"; //Toteutuksen osallistujat
   teksti += "<ul>";
   
   for (var i = 0; i < data.toteutus.osallistujat.nimet.length; i++) {
-    teksti += "<li>" + data.toteutus.osallistujat.nimet[i] + "</li>";
+    teksti += "<li>" + data.toteutus.osallistujat.nimet[i] + "</li>"; //Lista osallistujista
   }
   teksti += "</ul>";
-  
-  teksti += "<p>Alkaa: " + new Date(data.toteutus.alkamisAika).toLocaleDateString("fi-FI") + "</p>";
-  teksti += "<p>Päättyy: " + new Date(data.toteutus.loppumisAika).toLocaleDateString("fi-FI") + "</p>";
-  teksti += "<p>Kesto: " + data.toteutus.kestoViikoissa + " viikkoa</p>";
-  teksti += "<img src='" + data.toteutus.kuva + "' alt='Toteutuksen kuva'>";
+
+  teksti += "<p>Alkaa: " + new Date(data.toteutus.alkamisAika).toLocaleDateString("fi-FI") + "</p>"; //Kurssin alkamis ajankohta
+  teksti += "<p>Päättyy: " + new Date(data.toteutus.loppumisAika).toLocaleDateString("fi-FI") + "</p>"; //Päättymisen ajankohta
+  teksti += "<p>Kesto: " + data.toteutus.kestoViikoissa + " viikkoa</p>"; //Toteutuksen kesto viikkoina
+  teksti += "<img src='" + data.toteutus.kuva + "' alt='Toteutuksen kuva'>"; //Kuva toteutukseen liittyen
   
   document.getElementById("vastaus").innerHTML = teksti;
 }
